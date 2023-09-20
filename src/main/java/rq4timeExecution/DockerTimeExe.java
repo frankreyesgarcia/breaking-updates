9fa51195e102f83fb94e896f581a8cc90c076e12
Frank Reyes <frankrg@kth.se>
2023-09-20 22:38:12 +0200

    RQ4

--- src/main/java/rq4timeExecution/DockerTimeExe.java
+++ src/main/java/rq4timeExecution/DockerTimeExe.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -25,7 +26,7 @@ import java.util.Map;
 class Test {
     public static void main(String[] args) {
         DockerTimeExe dockerTimeExe = new DockerTimeExe();
-        dockerTimeExe.dockerMetadata(Path.of("/Users/frank/Desktop/commit"));
+        dockerTimeExe.dockerMetadata(Path.of("/Users/frank/Documents/Work/PHD/chains-project/fork/breaking-updates-fork/example"));
     }
 }
 
@@ -74,16 +75,25 @@ public class DockerTimeExe {
 
                 if (!results.containsKey((String) bu.get("breakingCommit"))) {
 
+                    Path logDirectory;
+                    try {
+                        logDirectory = Files.createDirectories(Path.of("executionTimeLogs")
+                                .resolve((String) bu.get("breakingCommit")));
+                    } catch (IOException e) {
+                        log.error("Could not create directories to copy the log file.");
+                        throw new RuntimeException(e);
+                    }
+
                     String prevImage = REGISTRY + ":" + bu.get("breakingCommit") + PRECEDING_COMMIT_CONTAINER_TAG;
                     String project = (String) bu.get("project");
 
-                    Map.Entry<String, ImageData> prevContainer = startContainer(prevImage, true, project);
+                    Map.Entry<String, ImageData> prevContainer = startContainer(prevImage, true, project,logDirectory);
                     if (prevContainer == null || prevContainer.getValue().imageSize == -1 || prevContainer.getValue().imageTime() == -1) {
                         continue;
                     }
 
                     String breakingImage = REGISTRY + ":" + bu.get("breakingCommit") + BREAKING_UPDATE_CONTAINER_TAG;
-                    Map.Entry<String, ImageData> breakingContainer = startContainer(breakingImage, false, project);
+                    Map.Entry<String, ImageData> breakingContainer = startContainer(breakingImage, false, project,logDirectory);
                     if (breakingContainer == null || breakingContainer.getValue().imageSize == -1 || breakingContainer.getValue().imageTime() == -1) {
                         continue;
                     }
@@ -106,7 +116,7 @@ public class DockerTimeExe {
         }
     }
 
-    private Map.Entry<String, ImageData> startContainer(String image, boolean isPrevImage, String project) {
+    private Map.Entry<String, ImageData> startContainer(String image, boolean isPrevImage, String project,Path logDirectory) {
         try {
             dockerClient.inspectImageCmd(image).exec();
         } catch (NotFoundException e) {
@@ -145,7 +155,11 @@ public class DockerTimeExe {
 
         System.out.println("Docker reproduce " + elapsedTime + " milisegundos. y size  " + size);
 
-//        removeImage(image, containerId);
+        String name = isPrevImage ? "prev" : "breaking";
+        storeLogFile(project, containerId, logDirectory,name);
+
+
+        //        removeImage(image, containerId);
 
         return Map.entry(containerId, imageData);
     }
@@ -179,5 +193,18 @@ public class DockerTimeExe {
         }
         dockerClient.removeContainerCmd(containerId).exec();
         dockerClient.removeImageCmd(image).withForce(true).exec();
+    }
+
+    private Path storeLogFile(String project, String containerId, Path outputDir,String logName) {
+        Path logOutputLocation = outputDir.resolve(logName+"CommitOutput.log");
+        String logLocation = "/%s/output.log".formatted(project);
+        try (InputStream logStream = dockerClient.copyArchiveFromContainerCmd(containerId, logLocation).exec()) {
+            byte[] fileContent = logStream.readAllBytes();
+            Files.write(logOutputLocation, fileContent);
+            return logOutputLocation;
+        } catch (IOException e) {
+            log.error("Could not store the log file for the project {}", project);
+            return null;
+        }
     }
 }
